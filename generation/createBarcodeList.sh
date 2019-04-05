d92aa9d79a3f80557119b660f7191e96b7d79878
Holger Cremer <HolgerCremer@gmail.com>
2019-04-05 18:01:28 +0200

    Several pdf generation improvements. - 2 scripts to create barcodes - "joined add" parameter to create partial lists for updates - a dockerfile to easy creation

---
+++ generation/createBarcodeList.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FOLDER=barcodes
+
+echo "Cleanup..."
+rm -rf $FOLDER
+mkdir -p $FOLDER
+
+echo "Create user list from db..."
+./query.sh $1
+
+echo "Make latex document and images..."
+ruby barcodelist.rb users.csv
+
+echo "Run pdflatex..."
+( cd $FOLDER && pdflatex barcode.latex )
--- generation/query.sh
+++ generation/query.sh
@@ -1,1 +1,17 @@
-sqlite3 shop.db "SELECT id,firstname,lastname FROM users LEFT JOIN authentication ON users.id = authentication.user WHERE (users.disabled IS NULL or users.disabled != 1) and id > 0" | sed "s~|~,~g" > users.csv
+#!/bin/bash
+
+if [[ "$1" != "" ]]; then
+  # format YYYY-MM-DD, e.g. 2017-01-01
+  DATE_PART="AND joined_at > strftime('%s', '$1')"
+fi
+
+SQL=`cat <<EOF
+SELECT id,firstname,lastname FROM users
+LEFT JOIN authentication ON users.id = authentication.user
+WHERE (users.disabled IS NULL or users.disabled != 1)
+AND id > 0
+$DATE_PART
+EOF
+`
+sqlite3 shop.db "$SQL" \
+  | sed "s~|~,~g" > users.csv
