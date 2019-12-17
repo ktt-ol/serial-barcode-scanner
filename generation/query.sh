c2ae4ed93c3e3d8d8c60b2e75bb9772dd8d0a529
[Malte Modler] <github@malte-modler.de>
2019-12-17 21:25:03 +0100

    update:       query.sh now the database is selected by the config file in the `etc/shopsystem/config.ini` but now root rights are required

--- generation/query.sh
+++ generation/query.sh
@@ -5,6 +5,11 @@ if [[ "$1" != "" ]]; then
   DATE_PART="AND joined_at > strftime('%s', '$1')"
 fi
 
+if [[ "$2" == "" ]]; then
+  # path to shop.db
+  PATH_TO_DB=`cat /etc/shopsystem/config.ini | grep file | cut -d '=' -f 2`
+fi
+
 SQL=`cat <<EOF
 SELECT id,firstname,lastname FROM users
 LEFT JOIN authentication ON users.id = authentication.user
@@ -13,5 +18,5 @@ AND id > 0
 $DATE_PART
 EOF
 `
-sqlite3 shop.db "$SQL" \
+sqlite3 $PATH_TO_DB "$SQL" \
   | sed "s~|~,~g" > users.csv
